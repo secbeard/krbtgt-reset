2bdfb963e5df99bfd85167e2b4121e7cc61ceac3
secbeard <63728517+secbeard@users.noreply.github.com>
2024-08-16 10:41:26 -0400

    Update krbtgt_reset.ps1

--- krbtgt_reset.ps1
+++ krbtgt_reset.ps1
@@ -1,4 +1,59 @@
-﻿# Pascal Bourbonnais 2024#region gen password# generate and confirm password function from: https://github.com/microsoftarchive/New-KrbtgtKeys.ps1/blob/master/New-KrbtgtKeys.ps1Function confirmPasswordIsComplex($pwd) {	Process {        $criteriaMet = 0		        # Upper Case Characters (A through Z, with diacritic marks, Greek and Cyrillic characters)        If ($pwd -cmatch '[A-Z]') {$criteriaMet++}		        # Lower Case Characters (a through z, sharp-s, with diacritic marks, Greek and Cyrillic characters)        If ($pwd -cmatch '[a-z]') {$criteriaMet++}		        # Numeric Characters (0 through 9)        If ($pwd -match '\d') {$criteriaMet++}		        # Special Chracters (Non-alphanumeric characters, currency symbols such as the Euro or British Pound are not counted as special characters for this policy setting)        If ($pwd -match '[\^~!@#$%^&*_+=`|\\(){}\[\]:;"''<>,.?/]') {$criteriaMet++}		        # Check If It Matches Default Windows Complexity Requirements        If ($criteriaMet -lt 3) {Return $false}        If ($pwd.Length -lt 8) {Return $false}        Return $true	}}Function generateNewComplexPassword([int]$passwordNrChars) {	Process {		$iterations = 0        Do {			If ($iterations -ge 20) {				Logging "  --> Complex password generation failed after '$iterations' iterations..." "ERROR"				Logging "" "ERROR"				EXIT			}			$iterations++			$pwdBytes = @()			$rng = New-Object System.Security.Cryptography.RNGCryptoServiceProvider			Do {				[byte[]]$byte = [byte]1				$rng.GetBytes($byte)				If ($byte[0] -lt 33 -or $byte[0] -gt 126) {					CONTINUE				}                $pwdBytes += $byte[0]			}			While ($pwdBytes.Count -lt $passwordNrChars)				$pwd = ([char[]]$pwdBytes) -join ''			}         Until (confirmPasswordIsComplex $pwd)        Return $pwd	}}
+﻿# Pascal Bourbonnais 2024
+
+#region gen password
+
+# generate and confirm password function from: https://github.com/microsoftarchive/New-KrbtgtKeys.ps1/blob/master/New-KrbtgtKeys.ps1
+
+Function confirmPasswordIsComplex($pwd) {
+    Process {
+        $criteriaMet = 0
+		
+        # Upper Case Characters (A through Z, with diacritic marks, Greek and Cyrillic characters)
+        If ($pwd -cmatch '[A-Z]') {$criteriaMet++}
+		
+        # Lower Case Characters (a through z, sharp-s, with diacritic marks, Greek and Cyrillic characters)
+        If ($pwd -cmatch '[a-z]') {$criteriaMet++}
+		
+        # Numeric Characters (0 through 9)
+        If ($pwd -match '\d') {$criteriaMet++}
+		
+        # Special Chracters (Non-alphanumeric characters, currency symbols such as the Euro or British Pound are not counted as special characters for this policy setting)
+        If ($pwd -match '[\^~!@#$%^&*_+=`|\\(){}\[\]:;"''<>,.?/]') {$criteriaMet++}
+		
+        # Check If It Matches Default Windows Complexity Requirements
+        If ($criteriaMet -lt 3) {Return $false}
+        If ($pwd.Length -lt 8) {Return $false}
+        Return $true
+    }
+}
+
+Function generateNewComplexPassword([int]$passwordNrChars) {
+    Process {
+        $iterations = 0
+        Do {
+            If ($iterations -ge 20) {
+                Logging "  --> Complex password generation failed after '$iterations' iterations..." "ERROR"
+                Logging "" "ERROR"
+                EXIT
+            }
+            $iterations++
+            $pwdBytes = @()
+            $rng = New-Object System.Security.Cryptography.RNGCryptoServiceProvider
+                Do {
+                [byte[]]$byte = [byte]1
+                $rng.GetBytes($byte)
+                    If ($byte[0] -lt 33 -or $byte[0] -gt 126) {
+                    CONTINUE
+                    }
+                $pwdBytes += $byte[0]
+                }
+            While ($pwdBytes.Count -lt $passwordNrChars)
+            $pwd = ([char[]]$pwdBytes) -join ''
+        } 
+        Until (confirmPasswordIsComplex $pwd)
+        Return $pwd
+    }
+}
 
 #endregion gen password
 
@@ -19,7 +74,17 @@ if ($env:COMPUTERNAME -eq $pdc.name) {
 
         $allRWDC = [System.Collections.ArrayList]@($allDC.Where({ ($_.OperationMasterRoles -notcontains "PDCEmulator") -and ($_.IsReadOnly -eq $false)}))
 
-        foreach ($dc in $allRWDC) {            #$replicationPartners = Get-ADReplicationPartnerMetadata -Target $dc -Scope Domain | Select-Object Server, LastReplicationSuccess            # Get replication failures            $repError = @(Get-ADReplicationFailure -Target $dc -Scope Domain | Select-Object Server, FirstFailureTime, FailureCount, FailureType)            if ($repError.Count -gt 0) {                 $proceed = $false                Write-EventLog -LogName Application -Source "pdcTasks" -EventId 3 -EntryType Error -Message "Replication failure detected with $($dc.HostName)"            }
+        foreach ($dc in $allRWDC) {
+            #$replicationPartners = Get-ADReplicationPartnerMetadata -Target $dc -Scope Domain | Select-Object Server, LastReplicationSuccess
+
+            # Get replication failures
+            $repError = @(Get-ADReplicationFailure -Target $dc -Scope Domain | Select-Object Server, FirstFailureTime, FailureCount, FailureType)
+
+            if ($repError.Count -gt 0) { 
+                $proceed = $false
+                Write-EventLog -LogName Application -Source "pdcTasks" -EventId 3 -EntryType Error -Message "Replication failure detected with $($dc.HostName)"
+            }
+
             # check if krbtgt passwordLastSet is equivalent
             $partnerKrbtgt = get-aduser krbtgt -Server $dc -Properties passwordLastSet
             if ($partnerKrbtgt.PasswordLastSet -ne $krbtgt.PasswordLastSet) {
